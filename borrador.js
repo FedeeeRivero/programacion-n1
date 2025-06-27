61480550f0a022ea4373cc0912dd99799c6722ab
Federico Rivero <federicorivero002@gmail.com>
2025-06-26 21:41:00 -0300

    parcial

---
+++ borrador.js
@@ -0,0 +1,29 @@
+
+
+function TemperaturaAleatorio(){
+    let min = 10
+    let max = 40
+    let resultado = 0
+    for(let i=1 ; i<=3; i++ ){
+        ropa[0]=math.floor(math.ramdon * (max-min+1)) + min
+        resultado = [4]
+
+    }console.log(resultado)
+}
+
+ TemperaturaAleatorio()
+
+function crearPrendaa(){
+ let ropa =["buzo", "pantalon", "remera", "medias", "interior"]
+ let secado = 0
+
+ for(let i =0; i>=ropa.length; i++ ){
+    if (secado>=100) {
+    console.log("La ropa esta seca");
+
+ }else ("La ropa sigue mojada")
+    
+
+
+}
+}
---
+++ parcial.js
@@ -0,0 +1,34 @@
+//simular 3 dias seguidos donde varias prendas se secan al sol
+// . cada prenda (5 en total) necesita llegar a 100 puntos de "secado"
+//: cada dia segun la temperatura y el viento , suman entre 10 y 40 puntos.
+// mostrar cuantos dias tarda cada prenda en secarse completamente.
+//funciones:crearPrenda; simularDia, actualizarSecado, mostrarResultado, etc
+
+
+//ejercicio 20 
+//SIMULAR 3 DIAS SEGUIDOS DONDE VARIAS PRENDAS SE SECAN AL SOL.
+
+function crearPrenda(){
+    let ropa =["buzo", "pantalon", "remera", "medias", "interior"]
+
+}console.log()
+}
+crearPrenda()
+
+function TemperaturaAleatorio(){
+    let min = 10
+    let max = 40
+
+    for (let i=0; i < array.length; i++) {
+         array[i]=math.floor(math.ramdon * (max-min+1)) + min
+    }
+   
+function SimularDia(){    
+let resultado = 0   
+for(let i=1 ; i<=3; i++ ){
+      resultado = TemperaturaAleatorio() + resultado
+
+    }console.log(resultado)
+}}
+
+SimularDia
